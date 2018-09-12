fd46613a9336ebcdde5113d2a42364ab001a34b9
baraboosal <salas@teisense.com>
2018-09-12 17:50:17 -0500

    Se agrega migracion con cambio a tabla persona para campo lastModification

--- baraboo/bsite/initialScript.py
+++ baraboo/bsite/initialScript.py
@@ -1,5 +1,16 @@
-from bsite.models import DocumentType
+from bsite.models import DocumentType, Role, Person
 
 doc = DocumentType(name="INE", description="Identificacion oficial")
-
 doc.save()
+
+rol = Role(roleName="Inversionista", roleDescription="ejemplo")
+rol.save()
+
+per = Person(
+    name="",
+    lastName="",
+    birdDate="",
+    lastModification="",
+    mail=""
+)
+per.save()
