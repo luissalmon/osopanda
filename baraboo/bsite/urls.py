ca40a91648221cbfb0b8aae823317c34cc34e541
Brandon Salinas <brandon@teisense.com>
2018-09-14 13:44:11 -0500

    Merge Dev with Creating Templates

--- baraboo/bsite/urls.py
+++ baraboo/bsite/urls.py
@@ -5,9 +5,8 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    path('login/investments.html', views.investmentspage, name = 'investments')
     path('', views.index, name = 'index'),
     path('login/', views.loginpage, name="login"),
-    path('provisionalName', LoginView.as_view(template_name="baraboo2.html"), name="provisionalName"),
     path('register/', views.formview, name="register"),
+    path('login/investments.html', views.investmentspage, name = 'investments')
 ]
