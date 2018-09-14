eb09780f9e63e6c2a039c6b54be5eddd1b7e8c15
Brandon Salinas <brandon@teisense.com>
2018-09-13 21:52:15 -0500

    Invest in process

--- baraboo/bsite/urls.py
+++ baraboo/bsite/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     #path('', views.index, name = 'index'),
     path('', LoginView.as_view(template_name="baraboo.html"), name="login"),
+    path('provisionalName', LoginView.as_view(template_name="baraboo2.html"), name="provisionalName"),
+    path('investments', LoginView.as_view(template_name="investments.html"), name="investments"),
     path('registrar', views.homepage, name = 'home')
 ]
